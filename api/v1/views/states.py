6f89778ff6ddf84ec5e738a79e68d267e97115b4
Boul3ez85 <benrebah.moez@gmail.com>
2020-09-09 17:33:59 +0100

    API

---
+++ api/v1/views/states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""create a new view to handle all default Restful API actions"""
+
+from flask import jsonify, abort, make_response, request
+from models import storage
+from models.state import State
+from api.v1.views import app_views
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_list():
+    """get information of states"""
+    li = []
+    for state in storage.all("State").values():
+        li.append(state.to_dict())
+    return jsonify(li)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_obj(state_id):
+    """get information a given state"""
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def del_obj(state_id):
+    """
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    state.delete()
+    storage.save()
+    return jsonify({})
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_obj():
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'name'not in request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    state = State(**request.get_json())
+    state.save()
+    return make_response(jsonify(state.to_dict()), 201)
