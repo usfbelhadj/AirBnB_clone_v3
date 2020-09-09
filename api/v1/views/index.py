da330be861eade3a6becdb09698ed40d7b60e1b7
Boul3ez85 <benrebah.moez@gmail.com>
2020-09-09 13:51:38 +0100

    get&count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,32 @@
 
 from api.v1.views import app_views
 from flask import jsonify, Flask, Blueprint
+from models import storage
+
+
+dbClass = {
+  "amenities": 47,
+  "cities": 36,
+  "places": 154,
+  "reviews": 718,
+  "states": 27,
+  "users": 31
+}
 
 
 @app_views.route('/status', strict_slashes=False)
 def objStatus():
     """return the status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def ret_cls():
+    ret_dic = {}
+    for k, v in dbClass.items():
+        ret_dic[k] = storage.count(v)
+    return jsonify(ret_dic)
+
+
+if __name__ == "__main__":
+    pass
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,20 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """get the class name and id"""
+        from models import storage
+        objs = storage.all(cls)
+        for obj in objs.values():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        from models import storage
+        if cls:
+            return len(storage.all(cls))
+        else:
+            return len(storage.all())
