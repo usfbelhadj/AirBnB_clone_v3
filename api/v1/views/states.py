38bb92e67818b6a6074fb24d6df215cd9c2a6d82
Boul3ez85 <https://github.com/Boul3ez85>
2020-09-09 20:55:18 +0100

    API methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -25,9 +25,10 @@ def get_obj(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def del_obj(state_id):
-    """
+    """delete object with its id"""
     state = storage.get("State", state_id)
     if state is None:
         abort(404)
@@ -35,12 +36,27 @@ def del_obj(state_id):
     storage.save()
     return jsonify({})
 
+
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_obj():
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'name'not in request.get_json():
+    """create an object"""
+    if not request.get_json() or if 'name' not in request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     state = State(**request.get_json())
     state.save()
     return make_response(jsonify(state.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_obj():
+    """update an existing object"""
+    state = storage.get('State', state_id)
+    if state is None:
+        abort(404)
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    for k, v in request.get_json().items():
+        if k not ['id', 'created_at', 'updated_at']:
+            setattr(state, k, v)
+    state.save()
+    return jsonify(state.to_dict())
