51a8aa140a0377a493139a1baeb6fb2caca4ce64
Boul3ez85 <benrebah.moez@gmail.com>
2020-09-10 16:15:46 +0100

    fix code

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -38,25 +38,15 @@ def del_obj(state_id):
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-def create_obj():
-    """create a new place object"""
-    city = storage.get("City", city_id)
-    user = storage.get("User", kwargs['user_id'])
-    if city is None:
-        abort(404)
+def post_state():
+    """create a new state object"""
     if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    kwargs = request.get_json()
-    if 'user_id' not in kwargs:
-        return make_response(jsonify({'error': 'Missing user_id'}), 400)
-    if user is None:
-        abort(404)
-    if 'name' not in kwargs:
+    if 'name' not in request.get_json():
         return make_response(jsonify({'error': 'Missing name'}), 400)
-    kwargs['city_id'] = city_id
-    place = Place(**kwargs)
-    place.save()
-    return make_response(jsonify(place.to_dict()), 201)
+    state = State(**request.get_json())
+    state.save()
+    return make_response(jsonify(state.to_dict()), 201)
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
