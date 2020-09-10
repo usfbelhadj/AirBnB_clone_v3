9a1405b92c9cd395f6d10c415218345ff0f6e6b6
Boul3ez85 <benrebah.moez@gmail.com>
2020-09-10 14:29:34 +0100

    handle __pycache__

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,12 @@ from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(error):
