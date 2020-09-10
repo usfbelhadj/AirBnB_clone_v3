82935107752e9a618718083374aff612de62a927
usfbelhadj <usf.belhadj@gmail.com>
2020-09-10 23:41:01 +0100

    Changee

--- api/v1/app.py
+++ api/v1/app.py
@@ -26,3 +26,16 @@ def not_found(error):
         er ([type]): [description]
     """
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.teardown_appcontext
+def close(self):
+    """"[calling close function]"
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    api_port = getenv('HBNB_API_PORT', default='5000')
+    app.run(host=api_host, port=int(api_port), threaded=True)
