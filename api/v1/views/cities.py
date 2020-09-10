ec2c7ee7f3bc1aabe29ac0f9cc193b803291921c
usfbelhadj <usf.belhadj@gmail.com>
2020-09-10 21:14:10 +0100

    Add model

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -72,7 +72,7 @@ def createcity(state_id):
     elif "name" not in ct.keys():
         abort(400, "Missing name")
     else:
-        new_state = State(**ct)
+        new_state = City(**ct)
         storage.new(new_state)
         storage.save()
         return jsonify(new_state.to_dict()), 201
