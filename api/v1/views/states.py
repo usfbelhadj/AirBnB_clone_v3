a3e71e3ec98f710fe235cd9bdc9ea0b269ca5280
Boul3ez85 <benrebah.moez@gmail.com>
2020-09-10 08:10:43 +0100

    test

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """create a new view to handle all default Restful API actions"""
 
+from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models import storage
 from models.state import State
-from api.v1.views import app_views
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
