18ae45725909baeb7331ae2fc0a8b7c1df151182
usfbelhadj <usf.belhadj@gmail.com>
2020-09-10 23:33:00 +0100

    Update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,10 +9,13 @@ from models import storage
 from flask import Flask, make_response
 from os import getenv
 from flask import jsonify
-
+from flask_cors import CORS
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r'/*': {"origins": "0.0.0.0"}})
+swagger = Swagger(app)
 
 
 @app.errorhandler(404)
